c8f34119a4f4df01ba6a4eaa7202215bdeba5842
marcgrant21 <marcgrant10@gmail.com>
2021-03-23 17:48:48 -0500

    final test

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or 'postgresql://project1:kkiss@localhost/project1'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL') or 'postgres://eudilppvcoxgnm:be5503240f7b5460a63ec5cd2433cef52cefafc5762909e307c524b228d0496c@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dbe6vdabpc249s'
+        'DATABASE_URL') or 'postgresql://eudilppvcoxgnm:be5503240f7b5460a63ec5cd2433cef52cefafc5762909e307c524b228d0496c@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dbe6vdabpc249s'
     # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     UPLOAD_FOLDER = os.environ['UPLOAD_FOLDER'] = 'uploads'
